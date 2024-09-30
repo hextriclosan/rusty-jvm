cbb354e5b698abd18edefa3c2861ce26615152e0
Igor Rudenko <iostream64@gmail.com>
2024-09-30 16:00:11 +0300

    Add support for One Interface Extends Another

---
+++ tests/test_data/OneInterfaceExtendsAnother.java
@@ -0,0 +1,31 @@
+package samples.inheritance.interfaces.oneinterfaceextendsanother;
+
+public class OneInterfaceExtendsAnother {
+    public static void main(String[] args) {
+        SubtractionLongs subtraction = new SubtractionImpl();
+
+        int one = 100;
+        int two = 300;
+        long result = subtraction.sub(one, two) + subtraction.subLong(one, two);
+    }
+}
+
+interface Subtraction {
+    int sub(int first, int second);
+}
+
+interface SubtractionLongs extends Subtraction {
+    long subLong(long first, long second);
+}
+
+class SubtractionImpl implements SubtractionLongs {
+    @Override
+    public int sub(int first, int second) {
+        return first - second;
+    }
+
+    @Override
+    public long subLong(long first, long second) {
+        return first - second;
+    }
+}
