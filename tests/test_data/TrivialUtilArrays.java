8e8e5135589e0d2026ebc7d30bada05f6732086d
Igor Rudenko <iostream64@gmail.com>
2024-10-10 15:54:46 +0300

    Add test on Arrays.binarySearch

---
+++ tests/test_data/TrivialUtilArrays.java
@@ -0,0 +1,10 @@
+package samples.javabase.util.arrays.trivial;
+
+import java.util.Arrays;
+
+public class TrivialUtilArrays {
+    public static void main(String[] args) {
+        int[] arr = new int[] {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+        int result =  Arrays.binarySearch(arr, 100);
+    }
+}
