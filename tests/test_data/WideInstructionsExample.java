47b2e58a799feefa2d6ab721959374eaa8ce3c11
Igor Rudenko <iostream64@gmail.com>
2024-10-30 16:05:56 +0200

    Fixes and improvements in WIDE instructions

---
+++ tests/test_data/WideInstructionsExample.java
@@ -0,0 +1,80 @@
+package samples.javacore.wide.instructions.trivial;
+
+public class WideInstructionsExample {
+    public static void main(String[] args) {
+        int result = getResult();
+    }
+
+    static int getResult() {
+        int padding0 = 0, padding1 = 0, padding2 = 0, padding3 = 0, padding4 = 0, padding5 = 0, padding6 = 0, padding7 = 0, padding8 = 0, padding9 = 0;
+        int padding10 = 0, padding11 = 0, padding12 = 0, padding13 = 0, padding14 = 0, padding15 = 0, padding16 = 0, padding17 = 0, padding18 = 0, padding19 = 0;
+        int padding20 = 0, padding21 = 0, padding22 = 0, padding23 = 0, padding24 = 0, padding25 = 0, padding26 = 0, padding27 = 0, padding28 = 0, padding29 = 0;
+        int padding30 = 0, padding31 = 0, padding32 = 0, padding33 = 0, padding34 = 0, padding35 = 0, padding36 = 0, padding37 = 0, padding38 = 0, padding39 = 0;
+        int padding40 = 0, padding41 = 0, padding42 = 0, padding43 = 0, padding44 = 0, padding45 = 0, padding46 = 0, padding47 = 0, padding48 = 0, padding49 = 0;
+        int padding50 = 0, padding51 = 0, padding52 = 0, padding53 = 0, padding54 = 0, padding55 = 0, padding56 = 0, padding57 = 0, padding58 = 0, padding59 = 0;
+        int padding60 = 0, padding61 = 0, padding62 = 0, padding63 = 0, padding64 = 0, padding65 = 0, padding66 = 0, padding67 = 0, padding68 = 0, padding69 = 0;
+        int padding70 = 0, padding71 = 0, padding72 = 0, padding73 = 0, padding74 = 0, padding75 = 0, padding76 = 0, padding77 = 0, padding78 = 0, padding79 = 0;
+        int padding80 = 0, padding81 = 0, padding82 = 0, padding83 = 0, padding84 = 0, padding85 = 0, padding86 = 0, padding87 = 0, padding88 = 0, padding89 = 0;
+        int padding90 = 0, padding91 = 0, padding92 = 0, padding93 = 0, padding94 = 0, padding95 = 0, padding96 = 0, padding97 = 0, padding98 = 0, padding99 = 0;
+        int padding100 = 0, padding101 = 0, padding102 = 0, padding103 = 0, padding104 = 0, padding105 = 0, padding106 = 0, padding107 = 0, padding108 = 0, padding109 = 0;
+        int padding110 = 0, padding111 = 0, padding112 = 0, padding113 = 0, padding114 = 0, padding115 = 0, padding116 = 0, padding117 = 0, padding118 = 0, padding119 = 0;
+        int padding120 = 0, padding121 = 0, padding122 = 0, padding123 = 0, padding124 = 0, padding125 = 0, padding126 = 0, padding127 = 0, padding128 = 0, padding129 = 0;
+        int padding130 = 0, padding131 = 0, padding132 = 0, padding133 = 0, padding134 = 0, padding135 = 0, padding136 = 0, padding137 = 0, padding138 = 0, padding139 = 0;
+        int padding140 = 0, padding141 = 0, padding142 = 0, padding143 = 0, padding144 = 0, padding145 = 0, padding146 = 0, padding147 = 0, padding148 = 0, padding149 = 0;
+        int padding150 = 0, padding151 = 0, padding152 = 0, padding153 = 0, padding154 = 0, padding155 = 0, padding156 = 0, padding157 = 0, padding158 = 0, padding159 = 0;
+        int padding160 = 0, padding161 = 0, padding162 = 0, padding163 = 0, padding164 = 0, padding165 = 0, padding166 = 0, padding167 = 0, padding168 = 0, padding169 = 0;
+        int padding170 = 0, padding171 = 0, padding172 = 0, padding173 = 0, padding174 = 0, padding175 = 0, padding176 = 0, padding177 = 0, padding178 = 0, padding179 = 0;
+        int padding180 = 0, padding181 = 0, padding182 = 0, padding183 = 0, padding184 = 0, padding185 = 0, padding186 = 0, padding187 = 0, padding188 = 0, padding189 = 0;
+        int padding190 = 0, padding191 = 0, padding192 = 0, padding193 = 0, padding194 = 0, padding195 = 0, padding196 = 0, padding197 = 0, padding198 = 0, padding199 = 0;
+        int padding200 = 0, padding201 = 0, padding202 = 0, padding203 = 0, padding204 = 0, padding205 = 0, padding206 = 0, padding207 = 0, padding208 = 0, padding209 = 0;
+        int padding210 = 0, padding211 = 0, padding212 = 0, padding213 = 0, padding214 = 0, padding215 = 0, padding216 = 0, padding217 = 0, padding218 = 0, padding219 = 0;
+        int padding220 = 0, padding221 = 0, padding222 = 0, padding223 = 0, padding224 = 0, padding225 = 0, padding226 = 0, padding227 = 0, padding228 = 0, padding229 = 0;
+        int padding230 = 0, padding231 = 0, padding232 = 0, padding233 = 0, padding234 = 0, padding235 = 0, padding236 = 0, padding237 = 0, padding238 = 0, padding239 = 0;
+        int padding240 = 0, padding241 = 0, padding242 = 0, padding243 = 0, padding244 = 0, padding245 = 0, padding246 = 0, padding247 = 0, padding248 = 0, padding249 = 0;
+        int padding250 = 0, padding251 = 0, padding252 = 0, padding253 = 0, padding254 = 0, padding255 = 0;
+
+        int i256 = 0;
+        float f256 = 0.0f;
+        double d256 = 0.0;
+        long l256 = 0L;
+        Object a256 = new Object();
+
+        i256 = 10;
+        f256 = 10.5f;
+        d256 = 20.5;
+        l256 = 10000000000L;
+        a256 = "Wide test";
+
+        i256 += 5; // iinc with WIDE for large increment or index
+        int localCopyInt = i256; // iload with WIDE
+        float localCopyFloat = f256; // fload with WIDE
+        double localCopyDouble = d256; // dload with WIDE
+        long localCopyLong = l256; // lload with WIDE
+        Object localCopyObject = a256; // aload with WIDE
+
+        // Using stores with WIDE (will be optimized by javac if not used)
+        localCopyInt += 5;  // iload, iinc, and istore with WIDE
+        localCopyFloat += 5.5f;  // fload, fstore with WIDE
+        localCopyDouble += 10.5; // dload, dstore with WIDE
+        localCopyLong += 1000000L; // lload, lstore with WIDE
+        localCopyObject = "Updated wide test"; // aload, astore with WIDE
+
+        int bit0 = localCopyInt == 20 ? 1 : 0;
+        int bit1 = localCopyFloat > 15.999f && localCopyFloat < 16.001f ? 1 : 0;
+        int bit2 = localCopyDouble > 30.999 && localCopyDouble < 31.001 ? 1 : 0;
+        int bit3 = localCopyLong == 10001000000L ? 1 : 0;
+        int bit4 = "Updated wide test".equals(localCopyObject) ? 1 : 0;
+
+        int result = 0;
+        result = setBit(result, 0, bit0);
+        result = setBit(result, 1, bit1);
+        result = setBit(result, 2, bit2);
+        result = setBit(result, 3, bit3);
+        result = setBit(result, 4, bit4);
+        return result;
+    }
+
+    private static int setBit(int num, int position, int value) {
+        return value == 0 ? num & ~(1 << position) : num | (1 << position);
+    }
+}
