d677f29d51ff58e486e3f9941f551e708e3ade28
Igor Rudenko <iostream64@gmail.com>
2024-11-03 18:08:13 +0200

    Add clone support for 1d-array objects

--- tests/should_clone_cloneables.rs
+++ tests/should_clone_cloneables.rs
@@ -8,5 +8,5 @@ fn should_clone_cloneables() {
     let last_frame_value = vm
         .run("samples.javacore.cloneable.trivial.CloneExample")
         .unwrap();
-    assert_eq!(7, get_int(last_frame_value))
+    assert_eq!(511, get_int(last_frame_value))
 }
--- tests/test_data/CloneExample.java
+++ tests/test_data/CloneExample.java
@@ -10,10 +10,41 @@ public class CloneExample {
         anotherCloneable.stringField = "second";
         int bit2 = cloneable.intField == 42 && cloneable.stringField.equals("first") ? 1 : 0;
 
+        int[] intArray = new int[]{1, 2, 3};
+        int[] anotherIntArray = intArray.clone();
+        int bit3 = intArray != anotherIntArray ? 1 : 0;
+        int bit4 = intArray[0] == anotherIntArray[0] && intArray[1] == anotherIntArray[1] && intArray[2] == anotherIntArray[2] ? 1 : 0;
+        anotherIntArray[1] = 200;
+        int bit5 = intArray[1] == 2 ? 1 : 0;
+
+        Object first = new Object(), second = new Object(), third = new Object();
+        Object[] objArray = new Object[]{first, second, third};
+        Object[] anotherObjArray = objArray.clone();
+        int bit6 = objArray != anotherObjArray ? 1 : 0;
+        int bit7 = objArray[0] == anotherObjArray[0] && objArray[1] == anotherObjArray[1] && objArray[2] == anotherObjArray[2] ? 1 : 0;
+        anotherObjArray[1] = new Object();
+        int bit8 = objArray[1] == second ? 1 : 0;
+
+//         int[][] intMatrix = new int[][]{{1, 2}, {3, 4}};
+//         int[][] anotherIntMatrix = intMatrix.clone();
+//         int bit9 = intMatrix != anotherIntMatrix ? 1 : 0;
+//         int bit10 = intMatrix[0] == anotherIntMatrix[0] && intMatrix[1] == anotherIntMatrix[1] ? 1 : 0;
+//         anotherIntMatrix[1][1] = 40;
+//         int bit11 = intMatrix[1][1] == 40 ? 1 : 0;
+
         int result = 0;
         result = setBit(result, 0, bit0);
         result = setBit(result, 1, bit1);
         result = setBit(result, 2, bit2);
+        result = setBit(result, 3, bit3);
+        result = setBit(result, 4, bit4);
+        result = setBit(result, 5, bit5);
+        result = setBit(result, 6, bit6);
+        result = setBit(result, 7, bit7);
+        result = setBit(result, 8, bit8);
+//         result = setBit(result, 9, bit9);
+//         result = setBit(result, 10, bit10);
+//         result = setBit(result, 11, bit11);
     }
 
     private static int setBit(int num, int position, int value) {
