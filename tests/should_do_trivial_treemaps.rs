ab8e42e5dc4ee97939c95d629b93c73bcf171b2a
Igor Rudenko <iostream64@gmail.com>
2024-11-05 21:42:40 +0200

    Improve Map tests

--- tests/should_do_trivial_treemaps.rs
+++ tests/should_do_trivial_treemaps.rs
@@ -8,5 +8,5 @@ fn should_do_trivial_treemaps() {
     let last_frame_value = vm
         .run("samples.javabase.util.treemap.trivial.TrivialTreeMap")
         .unwrap();
-    assert_eq!(84, get_int(last_frame_value))
+    assert_eq!(1, get_int(last_frame_value))
 }
