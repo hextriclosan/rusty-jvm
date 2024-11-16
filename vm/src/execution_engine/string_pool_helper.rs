d6d542e6a075cc85657854017472aca1f129dc50
Igor Rudenko <iostream64@gmail.com>
2024-11-16 16:36:50 +0200

    Refactoring: execute routines

--- vm/src/execution_engine/string_pool_helper.rs
+++ vm/src/execution_engine/string_pool_helper.rs
@@ -1,7 +1,5 @@
-use crate::error::Error;
-use crate::execution_engine::engine::Engine;
+use crate::execution_engine::executor::Executor;
 use crate::heap::heap::{with_heap_read_lock, with_heap_write_lock};
-use crate::method_area::method_area::with_method_area;
 
 pub struct StringPoolHelper {}
 
@@ -16,72 +14,30 @@ impl StringPoolHelper {
         Ok(string_ref)
     }
 
-    // refactor candidate A: generalize object creation and constructor invocation
     fn create_string(string: &str) -> crate::error::Result<i32> {
         if string.is_empty() {
             return Self::create_empty_string();
         }
 
         let codepoints = Self::string_to_codepoints(&string);
-
         let array_ref =
             with_heap_write_lock(|heap| heap.create_array_with_values("[I", &codepoints));
-        let string_class_name = "java/lang/String".to_string();
-        let instance_with_default_fields = with_method_area(|method_area| {
-            method_area.create_instance_with_default_fields(&string_class_name)
-        });
 
+        let args = vec![array_ref.into(), 0.into(), (codepoints.len() as i32).into()];
         let string_instance_ref =
-            with_heap_write_lock(|heap| heap.create_instance(instance_with_default_fields));
-
-        let full_signature = "<init>:([III)V";
-        let rc = with_method_area(|method_area| method_area.get(string_class_name.as_str()))?;
-        let special_method = rc
-            .methods
-            .method_by_signature
-            .get(full_signature)
-            .ok_or_else(|| Error::new_constant_pool(&format!("Error getting JavaMethod by class name {string_class_name} and full signature {full_signature} invoking special")))?;
-
-        let mut stack_frame = special_method.new_stack_frame()?;
-        stack_frame.set_local(0, string_instance_ref);
-        stack_frame.set_local(1, array_ref);
-        stack_frame.set_local(2, 0);
-        stack_frame.set_local(3, codepoints.len() as i32);
-
-        Engine::execute(stack_frame, &format!("creating \"{string}\""))?;
+            Executor::invoke_args_constructor("java/lang/String", "<init>:([III)V", &args)?;
 
         // todo: ensure that array_ref is collected by GC
         Ok(string_instance_ref)
     }
 
     // todo: consider creating all CPool strings like this
-    // refactor candidate A: generalize object creation and constructor invocation
     fn create_empty_string() -> crate::error::Result<i32> {
         let byte_array_ref =
             with_heap_write_lock(|heap| heap.create_array_with_values("[b", &vec![]));
-        let string_class_name = "java/lang/String".to_string();
-        let instance_with_default_fields = with_method_area(|method_area| {
-            method_area.create_instance_with_default_fields(&string_class_name)
-        });
-
+        let args = vec![byte_array_ref.into(), 0.into() /*coder LATIN1*/];
         let string_instance_ref =
-            with_heap_write_lock(|heap| heap.create_instance(instance_with_default_fields));
-
-        let full_signature = "<init>:([BB)V";
-        let rc = with_method_area(|method_area| method_area.get(string_class_name.as_str()))?;
-        let special_method = rc
-            .methods
-            .method_by_signature
-            .get(full_signature)
-            .ok_or_else(|| Error::new_constant_pool(&format!("Error getting JavaMethod by class name {string_class_name} and full signature {full_signature} invoking special")))?;
-
-        let mut stack_frame = special_method.new_stack_frame()?;
-        stack_frame.set_local(0, string_instance_ref);
-        stack_frame.set_local(1, byte_array_ref);
-        stack_frame.set_local(2, 0); // coder LATIN1
-
-        Engine::execute(stack_frame, "creating \"\"")?;
-
+            Executor::invoke_args_constructor("java/lang/String", "<init>:([BB)V", &args)?;
         Ok(string_instance_ref)
     }
 
--- vm/src/vm.rs
+++ vm/src/vm.rs
@@ -1,7 +1,6 @@
 use crate::error::Error;
-use crate::execution_engine::engine::Engine;
+use crate::execution_engine::executor::Executor;
 use crate::execution_engine::string_pool_helper::StringPoolHelper;
-use crate::heap::heap::with_heap_write_lock;
 use crate::method_area::method_area::{with_method_area, MethodArea};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -10,27 +9,15 @@ use tracing_subscriber::{fmt, EnvFilter};
 pub struct VM {}
 
 impl VM {
-    const ENTRY_POINT: &'static str = "main:([Ljava/lang/String;)V";
     const JAVA_HOME_ENV: &'static str = "RUSTY_JAVA_HOME";
 
     pub fn run(main_class_name: &str) -> crate::error::Result<Option<Vec<i32>>> {
         Self::prelude()?;
 
         let internal_name = &main_class_name.replace('.', "/");
-        let java_class = with_method_area(|area| area.get(internal_name))?;
-
-        let java_method = java_class
-            .methods
-            .method_by_signature
-            .get(Self::ENTRY_POINT)
-            .ok_or(Error::new_execution(
-                format!("main method not found in {main_class_name}").as_str(),
-            ))?;
-
-        Engine::execute(
-            java_method.new_stack_frame()?,
-            &format!("invoke {}", Self::ENTRY_POINT),
-        )
+        let result =
+            Executor::invoke_static_method(internal_name, "main:([Ljava/lang/String;)V", &[])?;
+        Ok(Some(result))
     }
 
     fn prelude() -> crate::error::Result<()> {
@@ -77,11 +64,12 @@ impl VM {
         Self::read_static_field("java/lang/ref/Reference", "processPendingActive")?;
 
         // create primordial ThreadGroup and Thread
-        let tg_obj_ref = Self::create_thread_group()?;
+        let tg_obj_ref = Executor::invoke_default_constructor("java/lang/ThreadGroup")?;
         let string_obj_ref = StringPoolHelper::get_string("system".to_string())?; // refactor candidate B: introduce and use here common string creator, not string pool one
-        let _thread_obj_ref = Self::create_thread(tg_obj_ref, string_obj_ref)?;
+        let _thread_obj_ref =
+            Executor::create_primordial_thread(&vec![tg_obj_ref.into(), string_obj_ref.into()])?;
 
-        Self::invoke_void_void_static_method("java/lang/System", "initPhase1:()V")?;
+        Executor::invoke_static_method("java/lang/System", "initPhase1:()V", &[])?;
 
         Ok(())
     }
@@ -93,87 +81,5 @@ impl VM {
 
         let raw_value = field.raw_value();
         Ok(raw_value)
-    }
-
-    // refactor candidate A: generalize object creation and constructor invocation
-    fn create_thread_group() -> crate::error::Result<i32> {
-        let tg_class_name = "java/lang/ThreadGroup".to_string();
-        let instance_with_default_fields = with_method_area(|method_area| {
-            method_area.create_instance_with_default_fields(&tg_class_name)
-        });
-
-        let tg_instance_ref =
-            with_heap_write_lock(|heap| heap.create_instance(instance_with_default_fields));
-
-        let full_signature = "<init>:()V";
-        let rc = with_method_area(|method_area| method_area.get(tg_class_name.as_str()))?;
-        let special_method = rc
-            .methods
-            .method_by_signature
-            .get(full_signature)
-            .ok_or_else(|| Error::new_constant_pool(&format!("Error getting JavaMethod by class name {tg_class_name} and full signature {full_signature} invoking special")))?;
-
-        let mut stack_frame = special_method.new_stack_frame()?;
-        stack_frame.set_local(0, tg_instance_ref);
-
-        Engine::execute(stack_frame, "invoking java/lang/ThreadGroup()")?;
-
-        Ok(tg_instance_ref)
-    }
-
-    // refactor candidate A: generalize object creation and constructor invocation
-    fn create_thread(tg_obj_ref: i32, string_obj_ref: i32) -> crate::error::Result<i32> {
-        let thread_class_name = "java/lang/Thread".to_string();
-        let instance_with_default_fields = with_method_area(|method_area| {
-            method_area.create_instance_with_default_fields(&thread_class_name)
-        });
-
-        let thread_instance_ref =
-            with_heap_write_lock(|heap| heap.create_instance(instance_with_default_fields));
-        with_method_area(|area| area.set_system_thread_id(thread_instance_ref)); //save primordial thread id
-
-        let full_signature = "<init>:(Ljava/lang/ThreadGroup;Ljava/lang/String;)V";
-        let rc = with_method_area(|method_area| method_area.get(thread_class_name.as_str()))?;
-        let special_method = rc
-            .methods
-            .method_by_signature
-            .get(full_signature)
-            .ok_or_else(|| Error::new_constant_pool(&format!("Error getting JavaMethod by class name {thread_class_name} and full signature {full_signature} invoking special")))?;
-
-        let mut stack_frame = special_method.new_stack_frame()?;
-        stack_frame.set_local(0, thread_instance_ref);
-        stack_frame.set_local(1, tg_obj_ref);
-        stack_frame.set_local(2, string_obj_ref);
-
-        Engine::execute(
-            stack_frame,
-            "invoking java/lang/Thread(ThreadGroup group, String name)",
-        )?;
-
-        Ok(thread_instance_ref)
-    }
-
-    // refactor candidate A: generalized action should be moved out of VM
-    fn invoke_void_void_static_method(
-        // refactor move me to utils
-        class_name: &str,
-        method_name: &str,
-    ) -> crate::error::Result<()> {
-        let java_class = with_method_area(|area| area.get(class_name))?;
-
-        let java_method = java_class
-            .methods
-            .method_by_signature
-            .get(method_name)
-            .ok_or(Error::new_execution(
-                format!("method {method_name} not found in {class_name}").as_str(),
-            ))?;
-
-        Engine::execute(
-            java_method.new_stack_frame()?,
-            &format!("invoke {class_name}.{method_name}"),
-        )?;
-
-        Ok(())
     }
 }
