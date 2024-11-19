3b97d1eccdce60da699f7f02a6ce2e0e3672971c
Igor Rudenko <iostream64@gmail.com>
2024-11-19 23:34:07 +0200

    Refactoring: control ops

--- vm/src/execution_engine/ops_control_processor.rs
+++ vm/src/execution_engine/ops_control_processor.rs
@@ -1,20 +1,23 @@
 use crate::error::Error;
 use crate::execution_engine::opcode::*;
+use crate::stack::sack_value::StackValue;
 use crate::stack::stack_frame::StackFrame;
+use std::fmt::Display;
 use tracing::trace;
 
 pub(crate) fn process(
     code: u8,
     stack_frames: &mut Vec<StackFrame>,
 ) -> crate::error::Result<Option<Vec<i32>>> {
-    let stack_frame = stack_frames.last_mut().unwrap();
     match code {
         GOTO => {
+            let stack_frame = last_frame(stack_frames)?;
             let offset = stack_frame.get_two_bytes_ahead();
             stack_frame.advance_pc(offset);
             trace!("GOTO -> offset={offset}");
         }
         LOOKUPSWITCH => {
+            let stack_frame = last_frame(stack_frames)?;
             let key: i32 = stack_frame.pop();
             let instruction_pc = stack_frame.pc() as i16;
             stack_frame.adjust_pc_to_4();
@@ -22,99 +25,64 @@ pub(crate) fn process(
             let default_offset = stack_frame.extract_four_bytes() as i16;
             let npairs = stack_frame.extract_four_bytes();
 
-            let mut match_found = false;
+            let mut matched_offset = None;
             for _ in 0..npairs {
                 let case_key = stack_frame.extract_four_bytes();
                 let offset = stack_frame.extract_four_bytes() as i16;
 
                 if key == case_key {
-                    let current_pc = stack_frame.pc() as i16;
-                    stack_frame.advance_pc(offset + instruction_pc - current_pc);
-                    match_found = true;
+                    //todo: add me to cache
+                    matched_offset = Some(offset);
                     break;
                 }
             }
 
-            if !match_found {
-                let current_pc = stack_frame.pc() as i16;
-                stack_frame.advance_pc(default_offset + instruction_pc - current_pc);
-            }
-
-            trace!("LOOKUPSWITCH -> default_offset={default_offset}, npairs={npairs}");
-        }
-        TABLESWITCH => {
-            let index: i32 = stack_frame.pop();
-            let instruction_pc = stack_frame.pc() as i16;
-            stack_frame.adjust_pc_to_4();
-
-            let default_offset = stack_frame.extract_four_bytes() as i16;
-            let low = stack_frame.extract_four_bytes();
-            let high = stack_frame.extract_four_bytes();
-
-            let offset_table = (low..=high)
-                .map(|_| stack_frame.extract_four_bytes())
-                .collect::<Vec<_>>();
-
-            let offset = offset_table
-                .get((index - low) as usize)
-                .map_or(default_offset, |offset| *offset as i16);
+            let offset = matched_offset.unwrap_or(default_offset);
             let current_pc = stack_frame.pc() as i16;
             stack_frame.advance_pc(offset + instruction_pc - current_pc);
 
-            trace!("TABLESWITCH -> default_offset={default_offset}, low={low}, high={high}");
+            trace!("LOOKUPSWITCH -> offset={offset}, npairs={npairs}");
+        }
+        TABLESWITCH => {
+            let stack_frame = last_frame(stack_frames)?;
+            let key_index: i32 = stack_frame.pop();
+            let instruction_pc = stack_frame.pc() as i16;
+            stack_frame.adjust_pc_to_4();
+
+            let default_offset = stack_frame.extract_four_bytes();
+            let from = stack_frame.extract_four_bytes();
+            let to = stack_frame.extract_four_bytes();
+
+            let offset_table = (from..=to)
+                .map(|_| stack_frame.extract_four_bytes())
+                .collect::<Vec<_>>(); //todo: add me to cache
+
+            let offset = offset_table
+                .get((key_index - from) as usize)
+                .copied()
+                .unwrap_or(default_offset);
+            let current_pc = stack_frame.pc() as i16;
+            stack_frame.advance_pc(offset as i16 + instruction_pc - current_pc);
+
+            trace!("TABLESWITCH -> offset={offset}, from={from}, to={to}");
         }
         IRETURN => {
-            let ret: i32 = stack_frame.pop();
-            stack_frames.pop();
-            let _frame = stack_frames
-                .last_mut()
-                .ok_or(Error::new_execution("Error getting stack last value"))?
-                .push(ret);
-            trace!("IRETURN -> ret={ret}");
+            perform_return::<i32>(stack_frames, "IRETURN")?;
         }
         LRETURN => {
-            let ret: i64 = stack_frame.pop();
-
-            stack_frames.pop();
-            let frame = stack_frames
-                .last_mut()
-                .ok_or(Error::new_execution("Error getting stack last value"))?;
-
-            frame.push(ret);
-            trace!("LRETURN -> ret={ret}");
+            perform_return::<i64>(stack_frames, "LRETURN")?;
         }
         FRETURN => {
-            let ret: f32 = stack_frame.pop();
-            stack_frames.pop();
-            stack_frames
-                .last_mut()
-                .ok_or(Error::new_execution("Error getting stack last value"))?
-                .push(ret);
-            trace!("FRETURN -> ret={ret}");
+            perform_return::<f32>(stack_frames, "FRETURN")?;
         }
         DRETURN => {
-            let ret: f64 = stack_frame.pop();
-
-            stack_frames.pop();
-            let frame = stack_frames
-                .last_mut()
-                .ok_or(Error::new_execution("Error getting stack last value"))?;
-
-            frame.push(ret);
-
-            trace!("DRETURN -> ret={ret}");
+            perform_return::<f64>(stack_frames, "DRETURN")?;
         }
         ARETURN => {
-            let objref: i32 = stack_frame.pop();
-
-            stack_frames.pop();
-            stack_frames
-                .last_mut()
-                .ok_or(Error::new_execution("Error getting stack last value"))?
-                .push(objref);
-            trace!("ARETURN -> objref={objref}");
+            perform_return::<i32>(stack_frames, "ARETURN")?;
         }
         RETURN => {
+            let stack_frame = last_frame(stack_frames)?;
             trace!("RETURN -> stack_frame.locals={:?}", stack_frame.locals());
             let last_value = Some(
                 stack_frames
@@ -137,4 +105,27 @@ pub(crate) fn process(
     }
 
     Ok(None)
+}
+
+fn last_frame(stack_frames: &mut Vec<StackFrame>) -> crate::error::Result<&mut StackFrame> {
+    stack_frames
+        .last_mut()
+        .ok_or(Error::new_execution("Error getting stack last frame"))
+}
+
+fn perform_return<T: StackValue + Copy + Display>(
+    stack_frames: &mut Vec<StackFrame>,
+    name: &str,
+) -> crate::error::Result<()> {
+    let stack_frame = last_frame(stack_frames)?;
+    let result: T = stack_frame.pop();
+
+    stack_frames.pop();
+    let next_frame = stack_frames
+        .last_mut()
+        .ok_or(Error::new_execution("Error getting stack last value"))?;
+    next_frame.push(result);
+
+    trace!("{name} -> {result}");
+    Ok(())
 }
