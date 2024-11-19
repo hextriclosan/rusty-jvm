a7adab56f69ab447b8e0fadf0e43a03c3b63c8da
Igor Rudenko <iostream64@gmail.com>
2024-11-19 22:32:14 +0200

    Refactoring: math ops

--- vm/src/execution_engine/ops_math_processor.rs
+++ vm/src/execution_engine/ops_math_processor.rs
@@ -1,347 +1,65 @@
 use crate::execution_engine::opcode::*;
+use crate::stack::sack_value::StackValue;
 use crate::stack::stack_frame::StackFrame;
+use std::fmt::Display;
 use tracing::trace;
 
 pub(crate) fn process(code: u8, stack_frames: &mut Vec<StackFrame>) -> crate::error::Result<()> {
     let stack_frame = stack_frames.last_mut().unwrap();
     match code {
-        IADD => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-            let result = a.wrapping_add(b);
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IADD -> {a} + {b} = {result}");
-        }
-        LADD => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a.wrapping_add(b);
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LADD -> {a} + {b} = {result}");
-        }
-        FADD => {
-            let b: f32 = stack_frame.pop();
-            let a: f32 = stack_frame.pop();
-            let result = a + b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("FADD -> {a} + {b} = {result}");
-        }
-        DADD => {
-            let b: f64 = stack_frame.pop();
-            let a: f64 = stack_frame.pop();
-
-            let result = a + b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("DADD -> {a} + {b} = {result}");
-        }
-        ISUB => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-            let result = a.wrapping_sub(b);
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("ISUB -> {a} - {b} = {result}");
-        }
-        LSUB => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a.wrapping_sub(b);
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LSUB -> {a} - {b} = {result}");
-        }
-        FSUB => {
-            let b: f32 = stack_frame.pop();
-            let a: f32 = stack_frame.pop();
-            let result = a - b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("FSUB -> {a} - {b} = {result}");
-        }
-        DSUB => {
-            let b: f64 = stack_frame.pop();
-            let a: f64 = stack_frame.pop();
-
-            let result = a - b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("DSUB -> {a} - {b} = {result}");
-        }
-        IMUL => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-            let result = a.wrapping_mul(b);
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IMUL -> {a} * {b} = {result}");
-        }
-        LMUL => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = b.wrapping_mul(a);
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LMUL -> {a} * {b} = {result}");
-        }
-        FMUL => {
-            let b: f32 = stack_frame.pop();
-            let a: f32 = stack_frame.pop();
-            let result = a * b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("FMUL -> {a} * {b} = {result}");
-        }
-        DMUL => {
-            let b: f64 = stack_frame.pop();
-            let a: f64 = stack_frame.pop();
-
-            let result = a * b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("DMUL -> {a} * {b} = {result}");
-        }
-        IDIV => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-            let result = a.wrapping_div(b); //todo add check for ArithmeticException here
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IDIV -> {a} / {b} = {result}");
-        }
-        LDIV => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a.wrapping_div(b); //todo add check for ArithmeticException here
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LDIV -> {a} / {b} = {result}");
-        }
-        FDIV => {
-            let b: f32 = stack_frame.pop();
-            let a: f32 = stack_frame.pop();
-            let result = a / b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("FDIV -> {a} / {b} = {result}");
-        }
-        DDIV => {
-            let b: f64 = stack_frame.pop();
-            let a: f64 = stack_frame.pop();
-
-            let result = a / b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("DDIV -> {a} / {b} = {result}");
-        }
-        IREM => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-            let result = a.wrapping_rem(b);
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IREM -> {a} % {b} = {result}");
-        }
-        LREM => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a.wrapping_rem(b);
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LREM -> {a} % {b} = {result}");
-        }
-        DREM => {
-            let b: f64 = stack_frame.pop();
-            let a: f64 = stack_frame.pop();
-
-            let result = a % b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("DREM -> {a} % {b} = {result}");
-        }
-        INEG => {
-            let value: i32 = stack_frame.pop();
-            let result = value.wrapping_neg();
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("INEG -> {result}");
-        }
-        LNEG => {
-            let value: i64 = stack_frame.pop();
-            let result = value.wrapping_neg();
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LNEG -> {result}");
-        }
-        ISHL => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-
-            let b_trunc = b & 0b00011111;
-            let result = a << b_trunc;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("ISHL -> {a} << {b} = {result}");
-        }
-        LSHL => {
-            let b: i32 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let b_trunc = b as u32 & 0b00111111u32;
-            let result = a << b_trunc;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LSHL -> {a} << {b} = {result}");
-        }
-        ISHR => {
-            // todo: recheck spec
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-
-            let b_trunc = b as u32 & 0b00011111u32;
-            let result = a >> b_trunc;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("ISHR -> {a} >> {b} = {result}");
-        }
-        LSHR => {
-            let b: i32 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let b_trunc = b as u32 & 0b00111111u32;
-            let result = a >> b_trunc;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LSHR -> {a} >> {b} = {result}");
-        }
-        IUSHR => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-
-            let b_trunc = b as u32 & 0b00011111u32;
-            let result = (a as u32 >> b_trunc) as i32;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IUSHR -> {a} >>> {b} = {result}");
-        }
-        LUSHR => {
-            let b: i32 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let b_trunc = b as u32 & 0b00111111u32;
-            let result = (a as u64 >> b_trunc) as i64;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LUSHR -> {a} >>> {b} = {result}");
-        }
-        IAND => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-
-            let result = a & b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IAND -> {a} & {b} = {result}");
-        }
-        LAND => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a & b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LAND -> {a} & {b} = {result}");
-        }
-        IOR => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-
-            let result = a | b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IOR -> {a} | {b} = {result}");
-        }
-        LOR => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a | b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LOR -> {a} | {b} = {result}");
-        }
-        IXOR => {
-            let b: i32 = stack_frame.pop();
-            let a: i32 = stack_frame.pop();
-
-            let result = a ^ b;
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("IXOR -> {a} & {b} = {result}");
-        }
-        LXOR => {
-            let b: i64 = stack_frame.pop();
-            let a: i64 = stack_frame.pop();
-
-            let result = a ^ b;
-
-            stack_frame.push(result);
-
-            stack_frame.incr_pc();
-            trace!("LXOR -> {a} & {b} = {result}");
-        }
+        IADD => binary_operation(stack_frame, |a: i32, b| a.wrapping_add(b), "IADD"),
+        LADD => binary_operation(stack_frame, |a: i64, b| a.wrapping_add(b), "LADD"),
+        FADD => binary_operation(stack_frame, |a: f32, b: f32| a + b, "FADD"),
+        DADD => binary_operation(stack_frame, |a: f64, b: f64| a + b, "DADD"),
+        ISUB => binary_operation(stack_frame, |a: i32, b| a.wrapping_sub(b), "ISUB"),
+        LSUB => binary_operation(stack_frame, |a: i64, b| a.wrapping_sub(b), "LSUB"),
+        FSUB => binary_operation(stack_frame, |a: f32, b: f32| a - b, "FSUB"),
+        DSUB => binary_operation(stack_frame, |a: f64, b: f64| a - b, "DSUB"),
+        IMUL => binary_operation(stack_frame, |a: i32, b| a.wrapping_mul(b), "IMUL"),
+        LMUL => binary_operation(stack_frame, |a: i64, b| a.wrapping_mul(b), "LMUL"),
+        FMUL => binary_operation(stack_frame, |a: f32, b: f32| a * b, "FMUL"),
+        DMUL => binary_operation(stack_frame, |a: f64, b: f64| a * b, "DMUL"),
+        IDIV => binary_operation(stack_frame, |a: i32, b| a.wrapping_div(b), "IDIV"), // todo add check for ArithmeticException here
+        LDIV => binary_operation(stack_frame, |a: i64, b| a.wrapping_div(b), "LDIV"), // todo add check for ArithmeticException here
+        FDIV => binary_operation(stack_frame, |a: f32, b: f32| a / b, "FDIV"),
+        DDIV => binary_operation(stack_frame, |a: f64, b: f64| a / b, "DDIV"),
+        IREM => binary_operation(stack_frame, |a: i32, b| a.wrapping_rem(b), "IREM"), // todo add check for ArithmeticException here
+        LREM => binary_operation(stack_frame, |a: i64, b| a.wrapping_rem(b), "LREM"), // todo add check for ArithmeticException here
+        DREM => binary_operation(stack_frame, |a: f64, b: f64| a % b, "DREM"),
+        INEG => unary_operation(stack_frame, |a: i32| a.wrapping_neg(), "INEG"),
+        LNEG => unary_operation(stack_frame, |a: i64| a.wrapping_neg(), "LNEG"),
+        ISHL => binary_operation(stack_frame, |a: i32, b: i32| a << (b & 0b00011111), "ISHL"),
+        LSHL => binary_operation(
+            stack_frame,
+            |a: i64, b: i32| a << (b as u32 & 0b00111111u32),
+            "LSHL",
+        ),
+        ISHR => binary_operation(
+            stack_frame,
+            |a: i32, b: i32| a >> (b as u32 & 0b00011111u32),
+            "ISHR",
+        ),
+        LSHR => binary_operation(
+            stack_frame,
+            |a: i64, b: i32| a >> (b as u32 & 0b00111111u32),
+            "LSHR",
+        ),
+        IUSHR => binary_operation(
+            stack_frame,
+            |a: i32, b: i32| (a as u32 >> (b as u32 & 0b00011111u32)) as i32,
+            "IUSHR",
+        ),
+        LUSHR => binary_operation(
+            stack_frame,
+            |a: i64, b: i32| (a as u64 >> (b as u32 & 0b00111111u32)) as i64,
+            "LUSHR",
+        ),
+        IAND => binary_operation(stack_frame, |a: i32, b: i32| a & b, "IAND"),
+        LAND => binary_operation(stack_frame, |a: i64, b: i64| a & b, "LAND"),
+        IOR => binary_operation(stack_frame, |a: i32, b: i32| a | b, "IOR"),
+        LOR => binary_operation(stack_frame, |a: i64, b: i64| a | b, "LOR"),
+        IXOR => binary_operation(stack_frame, |a: i32, b: i32| a ^ b, "IXOR"),
+        LXOR => binary_operation(stack_frame, |a: i64, b: i64| a ^ b, "LXOR"),
         IINC => {
             let index = stack_frame.extract_one_byte() as usize;
             let const_val = stack_frame.extract_one_byte() as i8;
@@ -362,4 +80,31 @@ pub(crate) fn process(code: u8, stack_frames: &mut Vec<StackFrame>) -> crate::er
     }
 
     Ok(())
+}
+
+fn unary_operation<T: StackValue + Copy + Display>(
+    stack_frame: &mut StackFrame,
+    op: impl Fn(T) -> T,
+    name: &str,
+) {
+    let value: T = stack_frame.pop();
+    let result = op(value);
+    stack_frame.push(result);
+
+    stack_frame.incr_pc();
+    trace!("{name} -> ({value})->{result}");
+}
+
+fn binary_operation<T1: StackValue + Copy + Display, T2: StackValue + Copy + Display>(
+    stack_frame: &mut StackFrame,
+    op: impl Fn(T1, T2) -> T1,
+    name: &str,
+) {
+    let b: T2 = stack_frame.pop();
+    let a: T1 = stack_frame.pop();
+    let result = op(a, b);
+    stack_frame.push(result);
+
+    stack_frame.incr_pc();
+    trace!("{name} -> ({a}, {b})->{result}");
 }
