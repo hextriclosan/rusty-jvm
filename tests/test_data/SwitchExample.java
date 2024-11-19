63ca20cc0c8e55bfc46b1ffbff67ae9377805179
Igor Rudenko <iostream64@gmail.com>
2024-11-19 23:34:07 +0200

    Refactoring: control ops

--- tests/test_data/SwitchExample.java
+++ tests/test_data/SwitchExample.java
@@ -3,18 +3,18 @@ package samples.javacore.switches.trivial;
 public class SwitchExample {
 
     public static void main(String[] args) {
-        int value = 30;
+        int value = 100_030;
 
         int lookupswitch = switch (value) {
-            case 10 -> 100;
-            case 20 -> 200;
-            case 30 -> 300;
+            case 100_010 -> 100;
+            case 100_020 -> 200;
+            case 100_030 -> 300;
             default -> -1;
         };
 
         int tableswitch = switch (value) {
-            case 29, 30, 31 -> 1000;
-            case 32, 33, 34 -> 2000;
+            case 100_029, 100_030, 100_031 -> 1000;
+            case 100_032, 100_033, 100_034 -> 2000;
             default -> -100;
         };
 
