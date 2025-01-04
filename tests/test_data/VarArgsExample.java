fb0751d1fe7501911813241974bd7a05b32a0548
Igor Rudenko <iostream64@gmail.com>
2025-01-04 13:43:48 +0200

    Add test for VarArgs

---
+++ tests/test_data/VarArgsExample.java
@@ -0,0 +1,23 @@
+package samples.javacore.varargs.trivial;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class  {
+    public static void main(String[] args) {
+        VarArgsExample varArgsExample = new VarArgsExample();
+        List<Integer> list = new ArrayList<>();
+        list.add(1337);
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(42, 1);
+        varArgsExample.varArgs(4, 1000_000_000_000L, 3.14, list, map);
+    }
+
+    private void varArgs(Object... args) {
+        for (Object arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
