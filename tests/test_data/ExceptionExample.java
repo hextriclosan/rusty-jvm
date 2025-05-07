49e458977824cf3fce64afe5d7e03a893782393e
Igor Rudenko <iostream64@gmail.com>
2025-05-07 18:39:41 +0300

    Polishing exception example

--- tests/test_data/ExceptionExample.java
+++ tests/test_data/ExceptionExample.java
@@ -1,31 +1,59 @@
 // javac -XDstringConcat=inline  -d . ExceptionExample.java
 package samples.javacore.exceptionexample;
 
+import java.util.List;
+
 public class ExceptionExample {
     public static void main(String[] args) {
         System.out.println("Beginning of main");
-        try {
-            System.out.println("Inside try block");
-            thrower();
-            System.out.println("Should be never reached");
-        } catch (NullPointerException e) {
-            System.out.println("Caught as NullPointerException: " + e);
-        } catch (RuntimeException e) {
-            System.out.println("Caught as RuntimeException: " + e);
-        } catch (Throwable e) {
-            System.out.println("Caught as Throwable: " + e);
-        }
-
-        try {
-            System.out.println("Inside another try block");
-            throw new Error();
-        } catch (Throwable e) {
-            System.out.println("Caught as Throwable second time: " + e);
+        List<Case> cases = List.of(new FewTriesInOneMethod());
+        for (Case aCase: cases) {
+            aCase.run();
         }
         System.out.println("End of main");
     }
+}
+
+abstract class Case {
+    private final String name = this.getClass().getSimpleName();
+    public void run() {
+        System.out.println("Running case: " + name);
+        runImpl();
+        System.out.println();
+    }
+
+    protected abstract void runImpl();
+
+    protected void print(String message) {
+        System.out.println("  " + message);
+    }
+
+}
+
+class FewTriesInOneMethod extends Case {
+    @Override
+    protected void runImpl() {
+        try {
+            print("Inside try block");
+            thrower();
+            print("Should be never reached");
+        } catch (NullPointerException e) {
+            print("Caught as NullPointerException: " + e);
+        } catch (RuntimeException e) {
+            print("Caught as RuntimeException: " + e);
+        } catch (Throwable e) {
+            print("Caught as Throwable: " + e);
+        }
+
+        try {
+            print("Inside another try block");
+            throw new IndexOutOfBoundsException("This is an index out of bounds exception");
+        } catch (Throwable e) {
+            print("Caught as Throwable second time: " + e);
+        }
+    }
 
     private static void thrower() {
-        throw new Error();
+        throw new Error("This is an error");
     }
 }
