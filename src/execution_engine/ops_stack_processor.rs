00990fa25ab2b5a1afa6ab33936d25c7b5e734cb
Igor Rudenko <iostream64@gmail.com>
2025-05-27 00:16:05 +0300

    Add support for dup2_x1 opcode

--- src/execution_engine/ops_stack_processor.rs
+++ src/execution_engine/ops_stack_processor.rs
@@ -60,6 +60,19 @@ pub(crate) fn process(code: u8, stack_frames: &mut StackFrames) -> crate::error:
             stack_frame.incr_pc();
             trace!("DUP2 -> value1={value1}, value2={value2}");
         }
+        DUP2_X1 => {
+            let value1: i32 = stack_frame.pop();
+            let value2: i32 = stack_frame.pop();
+            let value3: i32 = stack_frame.pop();
+            stack_frame.push(value2)?;
+            stack_frame.push(value1)?;
+            stack_frame.push(value3)?;
+            stack_frame.push(value2)?;
+            stack_frame.push(value1)?;
+
+            stack_frame.incr_pc();
+            trace!("DUP2_X1 -> value1={value1}, value2={value2}, value3={value3}");
+        }
         _ => {
             return Err(crate::error::Error::new_execution(&format!(
                 "Unknown stack opcode: {}",
