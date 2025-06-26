0bf6a6d4a6c8ab51c58a8141f47ac7ea0d93e1f1
Igor Rudenko <iostream64@gmail.com>
2025-06-27 00:21:56 +0300

    Add support of reading different endianess files

--- jimage-rs/src/bytes_utils.rs
+++ jimage-rs/src/bytes_utils.rs
@@ -1,36 +1,61 @@
 use crate::error::{JImageError, Result};
 
-pub(crate) fn read_integer<T: ReadFromBytes>(bytes: &[u8], offset: usize) -> Result<T> {
-    let from = offset;
-    let to = from + 4;
-    let arr: [u8; 4] = bytes[from..to]
-        .try_into()
-        .map_err(|_| JImageError::RawRead { from, to })?;
-    Ok(T::read(arr))
+pub(crate) fn read_integer<T: ReadFromBytes>(
+    bytes: &[u8],
+    offset: usize,
+    flipped: bool,
+) -> Result<T> {
+    read_integer_internal(bytes, offset, flipped, None)
 }
 
-pub(crate) fn read_integer_mut<T: ReadFromBytes>(bytes: &[u8], offset: &mut usize) -> Result<T> {
-    let from = *offset;
-    let to = from + 4;
-    let arr: [u8; 4] = bytes[from..to]
-        .try_into()
-        .map_err(|_| JImageError::RawRead { from, to })?;
-    *offset = to;
-    Ok(T::read(arr))
+pub(crate) fn read_integer_mut<T: ReadFromBytes>(
+    bytes: &[u8],
+    offset: &mut usize,
+    flipped: bool,
+) -> Result<T> {
+    read_integer_internal(bytes, *offset, flipped, Some(offset))
+}
+
+fn read_integer_internal<T: ReadFromBytes>(
+    bytes: &[u8],
+    from: usize,
+    flipped: bool,
+    offset_out: Option<&mut usize>,
+) -> Result<T> {
+    let size = T::SIZE;
+    let to = from + size;
+    let slice = bytes
+        .get(from..to)
+        .ok_or(JImageError::RawRead { from, to })?;
+
+    let mut buf = [0u8; 8];
+    buf[..size].copy_from_slice(slice);
+    if flipped {
+        buf[..size].reverse();
+    }
+
+    if let Some(offset) = offset_out {
+        *offset = to;
+    }
+
+    T::read(&buf[..size])
 }
 
 pub(crate) trait ReadFromBytes: Sized {
-    fn read(array: [u8; 4]) -> Self;
+    const SIZE: usize = size_of::<Self>();
+    fn read(bytes: &[u8]) -> Result<Self>;
 }
 
 impl ReadFromBytes for i32 {
-    fn read(array: [u8; 4]) -> Self {
-        Self::from_ne_bytes(array)
+    fn read(bytes: &[u8]) -> Result<Self> {
+        let arr: [u8; Self::SIZE] = bytes.try_into()?;
+        Ok(i32::from_ne_bytes(arr))
     }
 }
 
 impl ReadFromBytes for u32 {
-    fn read(array: [u8; 4]) -> Self {
-        Self::from_ne_bytes(array)
+    fn read(bytes: &[u8]) -> Result<Self> {
+        let arr: [u8; Self::SIZE] = bytes.try_into()?;
+        Ok(u32::from_ne_bytes(arr))
     }
 }
