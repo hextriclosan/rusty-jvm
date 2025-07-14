4bd49f6b14262053c1147448b6ddcf585f0bda8e
Igor Rudenko <iostream64@gmail.com>
2025-07-14 17:22:09 +0300

    Add support for native Unsafe.objectFieldOffset0(...)

--- tests/test_data/RandomExample.java
+++ tests/test_data/RandomExample.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class RandomExample {
     public static void main(String[] args) {
         long seed = -466210000;
-        Random random = new Random(seed); // Native Call Error: Native method java/lang/Class:getDeclaredFields0:(Z)[Ljava/lang/reflect/Field; not found
+        Random random = new Random(seed);
         System.out.println(random.nextInt());
     }
 }
