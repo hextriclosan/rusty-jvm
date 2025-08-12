1f8b590d770f399ee5aa65650e2f29a107b6ebc5
Igor Rudenko <iostream64@gmail.com>
2025-08-12 19:55:50 +0300

    Class.getDeclaredConstructors() - enable some previously implemented features in integration test

--- tests/test_data/DeclaredConstructorsExample.java
+++ tests/test_data/DeclaredConstructorsExample.java
@@ -5,28 +5,6 @@ import java.lang.reflect.Modifier;
 import java.util.Arrays;
 
 public class DeclaredConstructorsExample {
-
-    // Basic class with constructors
-    static class BasicExample {
-        public BasicExample() {
-        } // Public no-arg constructor
-
-        protected BasicExample(int number) {
-        } // Protected constructor
-
-        private BasicExample(String text) {
-        } // Private constructor
-    }
-
-    // Class with edge cases
-    abstract static class EdgeCaseExample {
-        EdgeCaseExample() {
-        } // Default package-private constructor
-
-        public EdgeCaseExample(String arg) throws IllegalArgumentException {
-        } // Constructor with exceptions
-    }
-
     public static void main(String[] args) throws Exception {
 
         // BASIC USAGE
@@ -40,22 +18,22 @@ public class DeclaredConstructorsExample {
         Constructor<?>[] edgeCaseConstructors = EdgeCaseExample.class.getDeclaredConstructors();
         printConstructorDetails(edgeCaseConstructors);
 
-        // HANDLING EXCEPTIONS jdk/internal/reflect/DirectMethodHandleAccessor$NativeAccessor:invoke0:(Ljava/lang/reflect/Method;Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object; not found
-//         System.out.println("Handle Constructor Exceptions:");
-//         try {
-//             Constructor<EdgeCaseExample> constructor = EdgeCaseExample.class.getDeclaredConstructor(String.class);
-//             printConstructorDetails(constructor);
-//         } catch (NoSuchMethodException e) {
-//             System.out.println("No such constructor found: " + e.getMessage());
-//         }
+        // HANDLING EXCEPTIONS
+        System.out.println("Handle Constructor Exceptions:");
+        try {
+            Constructor<EdgeCaseExample> constructor = EdgeCaseExample.class.getDeclaredConstructor(Float.class);
+            printConstructorDetails(constructor);
+        } catch (NoSuchMethodException e) {
+            System.out.println("No such constructor found: " + e.getMessage());
+        }
 
-        // ACCESS PRIVATE CONSTRUCTOR jdk/internal/reflect/DirectMethodHandleAccessor$NativeAccessor:invoke0:(Ljava/lang/reflect/Method;Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object; not found
-//         System.out.println("Access Private Constructor:");
-//         Constructor<BasicExample> privateConstructor = BasicExample.class.getDeclaredConstructor(String.class);
-//         printConstructorDetails(privateConstructor);
-//         privateConstructor.setAccessible(true); // Allow access
-//         BasicExample instance = privateConstructor.newInstance("Test");
-//         System.out.println("Private instance created: " + instance);
+        // ACCESS PRIVATE CONSTRUCTOR
+        System.out.println("Access Private Constructor:");
+        Constructor<BasicExample> privateConstructor = BasicExample.class.getDeclaredConstructor(String.class);
+        printConstructorDetails(privateConstructor);
+        privateConstructor.setAccessible(true); // Allow access
+        BasicExample instance = privateConstructor.newInstance("Test");
+        System.out.println("Private instance created: " + instance);
 
         // ANONYMOUS CLASSES
         System.out.println("Anonymous Class Constructors:");
@@ -66,23 +44,23 @@ public class DeclaredConstructorsExample {
 
     private static void printConstructorDetails(Constructor<?>[] constructors) {
         for (Constructor<?> constructor : constructors) {
-            System.out.println("\t----------------------------");
+            System.out.println("--------------------------------");
             printConstructorDetails(constructor);
         }
     }
 
     private static void printConstructorDetails(Constructor<?> constructor) {
-        //System.out.println("Constructor: " + constructor); // Native method jdk/internal/reflect/DirectMethodHandleAccessor$NativeAccessor:invoke0:(Ljava/lang/reflect/Method;Ljava/lang/Object;[Ljava/lang/Object;)Ljava/lang/Object; not found
-        System.out.println("\tParameter types: " + Arrays.toString(constructor.getParameterTypes()));
-        System.out.println("\tModifier: " + Modifier.toString(constructor.getModifiers()));
-        System.out.println("\tThrows: " + Arrays.toString(constructor.getExceptionTypes()));
-        //System.out.println(constructor.getAnnotatedReceiverType()); Native method java/lang/reflect/Executable:getTypeAnnotationBytes0:()[B not found
-        //System.out.println(constructor.getAnnotatedReturnType()); Native method java/lang/reflect/Executable:getTypeAnnotationBytes0:()[B not found
-        System.out.println("\tDeclaring class: " + constructor.getDeclaringClass());
+        System.out.println("Constructor:" + constructor);
+        System.out.println("  Parameter types:" + Arrays.toString(constructor.getParameterTypes()));
+        System.out.println("  Modifier:" + Modifier.toString(constructor.getModifiers()));
+        System.out.println("  Throws:" + Arrays.toString(constructor.getExceptionTypes()));
+        //System.out.println(constructor.getAnnotatedReceiverType()); //Native method java/lang/reflect/Executable:getTypeAnnotationBytes0:()[B not found
+        //System.out.println(constructor.getAnnotatedReturnType()); //Native method java/lang/reflect/Executable:getTypeAnnotationBytes0:()[B not found
+        System.out.println("  Declaring class:" + constructor.getDeclaringClass());
         //printArray(constructor.getGenericParameterTypes());
         //printArray(constructor.getGenericExceptionTypes());
-        System.out.println("\tName: " + constructor.getName());
-        System.out.println("\tParameter count: " + constructor.getParameterCount());
+        System.out.println("  Name:" + constructor.getName());
+        System.out.println("  Parameter count:" + constructor.getParameterCount());
     }
 
     private static void printArray(Object[] array) {
@@ -90,4 +68,30 @@ public class DeclaredConstructorsExample {
             System.out.println(" - " + obj);
         }
     }
+}
+
+// Basic class with constructors
+class BasicExample {
+    public BasicExample() {
+    } // Public no-arg constructor
+
+    protected BasicExample(int number) {
+    } // Protected constructor
+
+    private BasicExample(String text) {
+    } // Private constructor
+
+    @Override
+    public String toString() {
+        return "BasicExample{}";
+    }
+}
+
+// Class with edge cases
+abstract class EdgeCaseExample {
+    EdgeCaseExample() {
+    } // Default package-private constructor
+
+    public EdgeCaseExample(String arg) throws IllegalArgumentException {
+    } // Constructor with exceptions
 }
