28efbaf5f6ead5d5b449a7826f242980b61ede73
Igor Rudenko <iostream64@gmail.com>
2025-08-30 10:56:10 +0300

    java-25: Adjust to Class changes

--- src/vm/execution_engine/reflection_class_loader.rs
+++ src/vm/execution_engine/reflection_class_loader.rs
@@ -1,6 +1,8 @@
-use crate::vm::error::Result;
+use crate::vm::error::{Error, Result};
 use crate::vm::heap::heap::with_heap_write_lock;
+use crate::vm::helper::undecorate;
 use crate::vm::method_area::method_area::with_method_area;
+use crate::vm::method_area::primitives_helper::PRIMITIVE_TYPE_BY_CODE;
 use jdescriptor::TypeDescriptor;
 use std::collections::HashMap;
 use std::sync::RwLock;
@@ -12,39 +14,63 @@ pub struct ReflectionClassLoader {
 
 impl ReflectionClassLoader {
     pub fn load(&self, for_class: &str) -> Result<i32> {
+        let class_modifiers = with_method_area(|area| {
+            Ok::<u16, Error>(area.get(for_class)?.class_modifiers().bits())
+        })?;
+
         let component_type_ref_empty = 0;
         if !for_class.starts_with('[') && !for_class.ends_with(';') {
-            return self.get_or_create(for_class, component_type_ref_empty);
+            return self.get_or_create(for_class, component_type_ref_empty, class_modifiers);
         }
 
         let descriptor: TypeDescriptor = for_class.parse()?;
         match &descriptor {
             TypeDescriptor::Array(value, dimension) => {
-                let mut array_ref =
-                    self.get_or_create(&value.as_ref().to_string(), component_type_ref_empty)?;
+                let component_ref_name = value.as_ref().to_string();
+                let component_name = undecorate(&component_ref_name);
+                let component_flags = with_method_area(|area| {
+                    Ok::<u16, Error>(area.get(&component_name)?.class_modifiers().bits())
+                })?;
+
+                let mut array_ref = self.get_or_create(
+                    &component_ref_name,
+                    component_type_ref_empty,
+                    component_flags,
+                )?;
 
                 for padding in (0..*dimension as usize).rev() {
-                    array_ref = self.get_or_create(&for_class[padding..], array_ref)?;
+                    let name = &for_class[padding..];
+                    array_ref = self.get_or_create(name, array_ref, class_modifiers)?;
                 }
 
                 Ok(array_ref)
             }
-            _ => self.get_or_create(for_class, component_type_ref_empty),
+            _ => self.get_or_create(for_class, component_type_ref_empty, class_modifiers),
         }
     }
 
-    fn get_or_create(&self, class_name: &str, component_type_ref: i32) -> Result<i32> {
+    fn get_or_create(
+        &self,
+        class_name: &str,
+        component_type_ref: i32,
+        modifiers: u16,
+    ) -> Result<i32> {
         let mut class_type_map = self.class_type_instance_by_name.write()?;
         if let Some(&reflection_ref) = class_type_map.get(class_name) {
             return Ok(reflection_ref);
         }
 
-        let reflection_ref = Self::create_reflection_instance(class_name, component_type_ref)?;
+        let reflection_ref =
+            Self::create_reflection_instance(class_name, component_type_ref, modifiers)?;
         class_type_map.insert(class_name.to_string(), reflection_ref);
         Ok(reflection_ref)
     }
 
-    fn create_reflection_instance(this_class_name: &str, component_type_ref: i32) -> Result<i32> {
+    fn create_reflection_instance(
+        this_class_name: &str,
+        component_type_ref: i32,
+        modifiers: u16,
+    ) -> Result<i32> {
         let mut reflection_instance = with_method_area(|method_area| {
             method_area.create_instance_with_default_fields("java/lang/Class")
         })?;
@@ -52,6 +78,20 @@ impl ReflectionClassLoader {
             "java/lang/Class",
             "componentType",
             vec![component_type_ref],
+        )?;
+        reflection_instance.set_field_value(
+            "java/lang/Class",
+            "primitive",
+            vec![if PRIMITIVE_TYPE_BY_CODE.contains_key(this_class_name) {
+                1
+            } else {
+                0
+            }],
+        )?;
+        reflection_instance.set_field_value(
+            "java/lang/Class",
+            "modifiers",
+            vec![modifiers as i32],
         )?;
 
         let reflection_reference =
