bbcd4aef3c3bce0b822d31736964f5a22b2d3109
Igor Rudenko <iostream64@gmail.com>
2025-09-13 20:56:56 +0300

    Handle NullPointerException for ALOAD-group opcodes

--- src/vm/execution_engine/ops_load_processor.rs
+++ src/vm/execution_engine/ops_load_processor.rs
@@ -1,43 +1,69 @@
 use crate::vm::error::{Error, Result};
+use crate::vm::exception::helpers::throw_null_pointer_exception_with_message;
+use crate::vm::exception::throwing_result::ThrowingResult;
 use crate::vm::execution_engine::common::last_frame_mut;
 use crate::vm::execution_engine::opcode::*;
 use crate::vm::heap::heap::with_heap_read_lock;
 use crate::vm::stack::stack_frame::{StackFrame, StackFrames};
 use crate::vm::stack::stack_value::StackValue;
+use crate::{throw_and_return, unwrap_or_return_err, unwrap_result_or_return_default};
 use std::fmt::Display;
 use tracing::trace;
 
 pub(crate) fn process(code: u8, stack_frames: &mut StackFrames) -> Result<()> {
-    let stack_frame = last_frame_mut(stack_frames)?;
     match code {
-        ILOAD => handle_pos_and_load::<i32>(stack_frame, "ILOAD "),
-        LLOAD => handle_pos_and_load::<i64>(stack_frame, "LLOAD "),
-        FLOAD => handle_pos_and_load::<f32>(stack_frame, "FLOAD "),
-        DLOAD => handle_pos_and_load::<f64>(stack_frame, "DLOAD "),
-        ALOAD => handle_pos_and_load::<i32>(stack_frame, "ALOAD "),
+        ILOAD => handle_pos_and_load::<i32>(last_frame_mut(stack_frames)?, "ILOAD "),
+        LLOAD => handle_pos_and_load::<i64>(last_frame_mut(stack_frames)?, "LLOAD "),
+        FLOAD => handle_pos_and_load::<f32>(last_frame_mut(stack_frames)?, "FLOAD "),
+        DLOAD => handle_pos_and_load::<f64>(last_frame_mut(stack_frames)?, "DLOAD "),
+        ALOAD => handle_pos_and_load::<i32>(last_frame_mut(stack_frames)?, "ALOAD "),
         ILOAD_0 | ILOAD_1 | ILOAD_2 | ILOAD_3 => {
-            handle_load::<i32, _>(stack_frame, code - ILOAD_0, "ILOAD_")
+            handle_load::<i32, _>(last_frame_mut(stack_frames)?, code - ILOAD_0, "ILOAD_")
         }
         LLOAD_0 | LLOAD_1 | LLOAD_2 | LLOAD_3 => {
-            handle_load::<i64, _>(stack_frame, code - LLOAD_0, "LLOAD_")
+            handle_load::<i64, _>(last_frame_mut(stack_frames)?, code - LLOAD_0, "LLOAD_")
         }
         FLOAD_0 | FLOAD_1 | FLOAD_2 | FLOAD_3 => {
-            handle_load::<f32, _>(stack_frame, code - FLOAD_0, "FLOAD_")
+            handle_load::<f32, _>(last_frame_mut(stack_frames)?, code - FLOAD_0, "FLOAD_")
         }
         DLOAD_0 | DLOAD_1 | DLOAD_2 | DLOAD_3 => {
-            handle_load::<f64, _>(stack_frame, code - DLOAD_0, "DLOAD_")
+            handle_load::<f64, _>(last_frame_mut(stack_frames)?, code - DLOAD_0, "DLOAD_")
         }
         ALOAD_0 | ALOAD_1 | ALOAD_2 | ALOAD_3 => {
-            handle_load::<i32, _>(stack_frame, code - ALOAD_0, "ALOAD_")
+            handle_load::<i32, _>(last_frame_mut(stack_frames)?, code - ALOAD_0, "ALOAD_")
         }
-        IALOAD => handle_array_load::<i32>(stack_frame, "IALOAD"),
-        LALOAD => handle_array_load::<i64>(stack_frame, "LALOAD"),
-        FALOAD => handle_array_load::<f32>(stack_frame, "FALOAD"),
-        DALOAD => handle_array_load::<f64>(stack_frame, "DALOAD"),
-        AALOAD => handle_array_load::<i32>(stack_frame, "AALOAD"),
-        BALOAD => handle_array_load::<i32>(stack_frame, "BALOAD"),
-        CALOAD => handle_array_load::<i32>(stack_frame, "CALOAD"),
-        SALOAD => handle_array_load::<i32>(stack_frame, "SALOAD"),
+        IALOAD => {
+            let result = handle_array_load::<i32>(stack_frames, "IALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        LALOAD => {
+            let result = handle_array_load::<i64>(stack_frames, "LALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        FALOAD => {
+            let result = handle_array_load::<f32>(stack_frames, "FALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        DALOAD => {
+            let result = handle_array_load::<f64>(stack_frames, "DALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        AALOAD => {
+            let result = handle_array_load::<i32>(stack_frames, "AALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        BALOAD => {
+            let result = handle_array_load::<i32>(stack_frames, "BALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        CALOAD => {
+            let result = handle_array_load::<i32>(stack_frames, "CALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
+        SALOAD => {
+            let result = handle_array_load::<i32>(stack_frames, "SALOAD");
+            Ok(unwrap_result_or_return_default!(result, ()))
+        }
         _ => Err(Error::new_execution(&format!(
             "Unknown load opcode: {}",
             code
@@ -70,18 +96,47 @@ where
 }
 
 fn handle_array_load<T: StackValue + Display + Copy>(
-    stack_frame: &mut StackFrame,
+    stack_frames: &mut StackFrames,
     name_starts: &str,
-) -> Result<()> {
-    let index = stack_frame.pop();
-    let arrayref: i32 = stack_frame.pop();
-    let raw_value = with_heap_read_lock(|heap| heap.get_array_value(arrayref, index))?;
+) -> ThrowingResult<()> {
+    let (arrayref, index) = {
+        let stack_frame = unwrap_or_return_err!(last_frame_mut(stack_frames));
+        let index: i32 = stack_frame.pop();
+        let arrayref: i32 = stack_frame.pop();
+        (arrayref, index)
+    };
+    if arrayref == 0 {
+        let array_type = unwrap_or_return_err!(type_by_aload(name_starts));
+        throw_and_return!(throw_null_pointer_exception_with_message(
+            &format!("Cannot load from {array_type} array because \"<VALUE>\" is null"),
+            stack_frames
+        ))
+    }
+    let raw_value = with_heap_read_lock(|heap| heap.get_array_value(arrayref, index));
+    let raw_value = unwrap_or_return_err!(raw_value);
 
     let value: T = T::from_vec(&raw_value);
 
-    stack_frame.push(value)?;
+    let stack_frame = unwrap_or_return_err!(last_frame_mut(stack_frames));
+    unwrap_or_return_err!(stack_frame.push(value));
     stack_frame.incr_pc();
     trace!("{name_starts} -> arrayref={arrayref}, index={index}, value={value}");
 
-    Ok(())
+    ThrowingResult::ok(())
+}
+
+fn type_by_aload(aload: &str) -> Result<&str> {
+    match aload {
+        "IALOAD" => Ok("int"),
+        "LALOAD" => Ok("long"),
+        "FALOAD" => Ok("float"),
+        "DALOAD" => Ok("double"),
+        "AALOAD" => Ok("object"),
+        "BALOAD" => Ok("byte"),
+        "CALOAD" => Ok("char"),
+        "SALOAD" => Ok("short"),
+        _ => Err(Error::new_execution(&format!(
+            "Unknown array load type: {aload}"
+        ))),
+    }
 }
