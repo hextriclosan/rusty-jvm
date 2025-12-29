a0eda6710e8405dbc6a824f4566c294adb726aba
Igor Rudenko <iostream64@gmail.com>
2025-12-29 15:36:39 +0200

    Move rust-jar to https://github.com/hextriclosan/rusty-jar

--- rusty-jar/CHANGELOG.md
+++
@@ -1,4 +0,0 @@
-# 0.0.1
-## What's Added
-Added support for
-* Basic functionality: read files from a JAR archive.
--- rusty-jar/src/lib.rs
+++
@@ -1,4 +0,0 @@
-//! A Rust library for handling JAR (Java ARchive) files.
-pub mod error;
-pub mod jarfile;
-pub use crate::jarfile::JarFile;
