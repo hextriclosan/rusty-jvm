fcb9e39178945165a59f1f46c8678d614d2426ae
Igor Rudenko <iostream64@gmail.com>
2026-03-18 00:42:05 +0200

    Integration with jni-sys - beginning

--- src/vm/jni/jni_impl.rs
+++ src/vm/jni/jni_impl.rs
@@ -1,7 +1,12 @@
 use crate::vm::jni::jni_env::JNIEnv;
+use jni_sys::{jboolean, jint, JNI_FALSE};
 
 const JNI_VERSION_24: i32 = 0x00180000;
 
-pub(super) extern "system" fn get_version(_env: *mut JNIEnv) -> i32 {
+pub(super) extern "system" fn get_version(_env: JNIEnv) -> jint {
     JNI_VERSION_24
+}
+
+pub(super) extern "system" fn exception_check(_env: JNIEnv) -> jboolean {
+    JNI_FALSE // todo: implement me
 }
