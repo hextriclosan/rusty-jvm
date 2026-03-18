c4cf9aa3448c9125d4c9c4513e1342215143dfcc
Igor Rudenko <iostream64@gmail.com>
2026-03-18 14:08:56 +0200

    Add stubs for jni_sys::JavaVM

--- src/vm/jni/jni_impl.rs
+++ src/vm/jni/jni_impl.rs
@@ -1,4 +1,5 @@
-use jni_sys::{jboolean, jint, JNIEnv, JNI_FALSE};
+use crate::vm::jni::jni_env::get_jni_vm;
+use jni_sys::{jboolean, jint, JNIEnv, JavaVM, JNI_FALSE};
 
 const JNI_VERSION_24: i32 = 0x00180000;
 
@@ -8,4 +9,12 @@ pub(super) extern "system" fn get_version(_env: *mut JNIEnv) -> jint {
 
 pub(super) extern "system" fn exception_check(_env: *mut JNIEnv) -> jboolean {
     JNI_FALSE // todo: implement me
+}
+
+pub(super) extern "system" fn get_java_vm(_env: *mut JNIEnv, vm: *mut *mut JavaVM) -> jint {
+    unsafe {
+        let jni_vm = get_jni_vm();
+        *vm = jni_vm;
+    }
+    0 // illustrates success
 }
