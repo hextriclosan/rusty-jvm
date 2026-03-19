75229dddd3a752ec0467b43a446f6f490c4353b7
Igor Rudenko <iostream64@gmail.com>
2026-03-19 23:12:46 +0200

    fix

--- tests/utils.rs
+++ tests/utils.rs
@@ -14,18 +14,10 @@ pub enum ExecutionResult {
 
 pub(crate) static REPO_PATH: Lazy<PathBuf> =
     Lazy::new(|| env::current_dir().expect("Failed to get current directory"));
-
-pub(crate) static TARGET_PATH: Lazy<PathBuf> = Lazy::new(|| {
-    let out_dir = PathBuf::from(env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".into()));
-    out_dir
-});
 pub(crate) static TEST_PATH: Lazy<PathBuf> = Lazy::new(|| {
-    let out_dir = TARGET_PATH.join("java_classes_for_tests");
+    let out_dir = PathBuf::from(env::var("CARGO_TARGET_DIR").unwrap_or_else(|_| "target".into()))
+        .join("java_classes_for_tests");
 
-    out_dir
-});
-pub(crate) static TEST_LIB_DIR_PATH: Lazy<PathBuf> = Lazy::new(|| {
-    let out_dir = TARGET_PATH.join("debug");
     out_dir
 });
 
