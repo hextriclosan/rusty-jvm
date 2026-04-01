60a21279fa69c8239edc295b3862b056c247cbd0
Igor Rudenko <iostream64@gmail.com>
2026-04-01 16:43:25 +0300

    fix

--- tests/jni_test_lib/src/array_operations_demo.rs
+++ tests/jni_test_lib/src/array_operations_demo.rs
@@ -1,10 +1,10 @@
-use jni::sys::{jarray, jclass, jint, JNIEnv};
+use jni::sys::{jclass, jint, jobject, JNIEnv};
 
 #[no_mangle]
 pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_GetArrayLength(
     env: *mut JNIEnv,
     _class: jclass,
-    input: jarray,
+    input: jobject,
 ) -> jint {
     unsafe { ((*(*env)).v24.GetArrayLength)(env, input) }
 }
