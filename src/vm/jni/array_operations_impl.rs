f6af154d776b3b605793131ee8a434fe40175faa
Igor Rudenko <iostream64@gmail.com>
2026-04-02 14:14:47 +0300

    Add support for JNI function New<PrimitiveType>Array(...)

--- src/vm/jni/array_operations_impl.rs
+++ src/vm/jni/array_operations_impl.rs
@@ -1,6 +1,9 @@
 use crate::vm::heap::heap::HEAP;
 use crate::vm::helper::klass;
-use jni_sys::{jarray, jclass, jint, jobject, jobjectArray, jsize, JNIEnv};
+use jni_sys::{
+    jarray, jbooleanArray, jbyteArray, jcharArray, jclass, jdoubleArray, jfloatArray, jint,
+    jintArray, jlongArray, jobject, jobjectArray, jshortArray, jsize, JNIEnv,
+};
 
 pub(super) extern "system" fn get_array_length(_env: *mut JNIEnv, input: jarray) -> jint {
     let array_ref = input as i32;
@@ -92,4 +95,44 @@ pub(super) extern "system" fn set_object_array_element(
 
     HEAP.set_array_value(array_ref, index as i32, vec![value as i32])
         .expect("Failed to set array element");
+}
+
+pub(super) extern "system" fn new_boolean_array(_env: *mut JNIEnv, len: jsize) -> jbooleanArray {
+    new_array_impl(len, "[Z") as jbooleanArray
+}
+
+pub(super) extern "system" fn new_byte_array(_env: *mut JNIEnv, len: jsize) -> jbyteArray {
+    new_array_impl(len, "[B") as jbyteArray
+}
+
+pub(super) extern "system" fn new_char_array(_env: *mut JNIEnv, len: jsize) -> jcharArray {
+    new_array_impl(len, "[C") as jcharArray
+}
+
+pub(super) extern "system" fn new_short_array(_env: *mut JNIEnv, len: jsize) -> jshortArray {
+    new_array_impl(len, "[S") as jshortArray
+}
+
+pub(super) extern "system" fn new_int_array(_env: *mut JNIEnv, len: jsize) -> jintArray {
+    new_array_impl(len, "[I") as jintArray
+}
+
+pub(super) extern "system" fn new_long_array(_env: *mut JNIEnv, len: jsize) -> jlongArray {
+    new_array_impl(len, "[J") as jlongArray
+}
+
+pub(super) extern "system" fn new_float_array(_env: *mut JNIEnv, len: jsize) -> jfloatArray {
+    new_array_impl(len, "[F") as jfloatArray
+}
+
+pub(super) extern "system" fn new_double_array(_env: *mut JNIEnv, len: jsize) -> jdoubleArray {
+    new_array_impl(len, "[D") as jdoubleArray
+}
+
+fn new_array_impl(len: jsize, type_name: &str) -> jobjectArray {
+    if len < 0 {
+        panic!("Negative array length: {len}"); // todo throw NegativeArraySizeException here
+    }
+
+    HEAP.create_array(&type_name, len) as jarray
 }
--- tests/jni_test_lib/src/array_operations_demo.rs
+++ tests/jni_test_lib/src/array_operations_demo.rs
@@ -1,4 +1,7 @@
-use jni::sys::{jclass, jint, jobject, JNIEnv};
+use jni::sys::{
+    jbooleanArray, jbyteArray, jcharArray, jclass, jdoubleArray, jfloatArray, jint, jintArray,
+    jlongArray, jobject, jshortArray, JNIEnv,
+};
 
 #[no_mangle]
 pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_GetArrayLength(
@@ -39,4 +42,76 @@ pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperations
     value: jobject,
 ) {
     unsafe { ((*(*env)).v24.SetObjectArrayElement)(env, array, index, value) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewBooleanArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jbooleanArray {
+    unsafe { ((*(*env)).v24.NewBooleanArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewByteArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jbyteArray {
+    unsafe { ((*(*env)).v24.NewByteArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewCharArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jcharArray {
+    unsafe { ((*(*env)).v24.NewCharArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewShortArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jshortArray {
+    unsafe { ((*(*env)).v24.NewShortArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewIntArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jintArray {
+    unsafe { ((*(*env)).v24.NewIntArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewLongArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jlongArray {
+    unsafe { ((*(*env)).v24.NewLongArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewFloatArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jfloatArray {
+    unsafe { ((*(*env)).v24.NewFloatArray)(env, length) }
+}
+
+#[no_mangle]
+pub extern "system" fn Java_samples_javacore_loadlibrary_example_ArrayOperationsDemo_NewDoubleArray(
+    env: *mut JNIEnv,
+    _class: jclass,
+    length: jint,
+) -> jdoubleArray {
+    unsafe { ((*(*env)).v24.NewDoubleArray)(env, length) }
 }
