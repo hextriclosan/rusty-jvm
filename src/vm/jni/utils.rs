58e5870ae5ad785a7b4f726b8223ba00d3183a32
Igor Rudenko <iostream64@gmail.com>
2026-04-07 00:38:10 +0300

    fix

--- src/vm/jni/utils.rs
+++ src/vm/jni/utils.rs
@@ -5,6 +5,6 @@ macro_rules! from_mutf8_ptr {
         // Convert the raw pointer to a byte slice
         let ptr = $ptr;
         assert!(!ptr.is_null(), "from_mutf8_ptr! received a null pointer");
-        cesu8::from_java_cesu8(unsafe { CStr::from_ptr($ptr) }.to_bytes())
+        cesu8::from_java_cesu8(unsafe { CStr::from_ptr(ptr) }.to_bytes())
     }};
 }
