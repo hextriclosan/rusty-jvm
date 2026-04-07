b7baddbb1bb981e4d0c088fc5d419e96d321206f
Igor Rudenko <iostream64@gmail.com>
2026-04-07 18:22:45 +0300

    Add integration test for enum with abstract method

---
+++ tests/test_data/TrickyEnumsExample.java
@@ -0,0 +1,36 @@
+package samples.javacore.enums.tricky;
+
+public class TrickyEnumsExample {
+
+    public static void main(String[] args) {
+        for (TrickyEnum te : TrickyEnum.values()) {
+            printEnumValues(te);
+        }
+    }
+
+    private static void printEnumValues(TrickyEnum trickyEnum) {
+        System.out.println(trickyEnum.name() + " = " + trickyEnum.getLongName());
+    }
+
+}
+
+enum TrickyEnum {
+    ONE {
+        @Override
+        String getLongName() {
+            return "I'm the ONE!";
+        }
+    }, TWO {
+        @Override
+        String getLongName() {
+            return "I'm the Second!";
+        }
+    }, THREE {
+        @Override
+        String getLongName() {
+            return "I'm a Looser";
+        }
+    };
+
+    abstract String getLongName();
+}
