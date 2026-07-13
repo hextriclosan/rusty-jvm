303d929a8c16b5cf68206f2b6e5efcacdb487411
Igor Rudenko <iostream64@gmail.com>
2026-07-13 12:27:37 -0700

    Refactor native methods in java.lang.invoke.MethodHandleNatives

--- src/vm/system_native/method_handle_natives/mod.rs
+++ src/vm/system_native/method_handle_natives/mod.rs
@@ -1,3 +1,12 @@
+use crate::vm::error::Result;
+use crate::vm::heap::heap::HEAP;
+use crate::vm::helper::clazz_ref;
+use crate::vm::system_native::method_handle_natives::member_name::MemberName;
+use crate::vm::system_native::method_handle_natives::offsets::{
+    get_field_offset, get_static_field_offset,
+};
+use crate::vm::system_native::method_handle_natives::resolution::member_name_init;
+
 pub(crate) mod invocation;
 mod member_name;
 mod method_type;
@@ -8,3 +17,72 @@ mod resolved_method_name;
 pub(crate) mod types;
 mod var_handle;
 pub(crate) mod wrappers;
+
+/// `java.lang.invoke.MethodHandleNatives.registerNatives()V`
+pub(crate) fn register_natives() -> Result<()> {
+    Ok(()) // todo: implement me
+}
+
+/// `java.lang.invoke.MethodHandleNatives.init(Ljava/lang/invoke/MemberName;Ljava/lang/Object;)V`
+pub(crate) fn init(member_name_ref: i32, obj_ref: i32) -> Result<()> {
+    member_name_init(member_name_ref, obj_ref)
+}
+
+/// `java.lang.invoke.MethodHandleNatives.resolve(Ljava/lang/invoke/MemberName;Ljava/lang/Class;IZ)Ljava/lang/invoke/MemberName;`
+pub(crate) fn resolve(
+    member_name_ref: i32,
+    caller_class_ref: i32,
+    lookup_mode: i32,
+    speculative_resolve: bool,
+) -> Result<i32> {
+    resolution::resolve(
+        member_name_ref,
+        caller_class_ref,
+        lookup_mode,
+        speculative_resolve,
+    )
+}
+
+/// `java.lang.invoke.MethodHandleNatives.objectFieldOffset(Ljava/lang/invoke/MemberName;)J`
+pub(crate) fn object_field_offset(member_name_ref: i32) -> Result<i64> {
+    get_field_offset(member_name_ref)
+}
+
+/// `java.lang.invoke.MethodHandleNatives.staticFieldOffset(Ljava/lang/invoke/MemberName;)J`
+pub(crate) fn static_field_offset(member_name_ref: i32) -> Result<i64> {
+    get_static_field_offset(member_name_ref)
+}
+
+/// `java.lang.invoke.MethodHandleNatives.staticFieldBase(Ljava/lang/invoke/MemberName;)Ljava/lang/Object;`
+pub(crate) fn static_field_base(member_name_ref: i32) -> Result<i32> {
+    let member_name = MemberName::new(member_name_ref)?;
+    let class_name = member_name.class_name();
+    clazz_ref(class_name)
+}
+
+/// `java.lang.invoke.MethodHandleNatives.getNamedCon(I[Ljava/lang/Object;)I`
+// By not setting a value for `box` we break the check loop on the Java side so, just bypassing the check
+pub(crate) fn get_named_con(_what: i32, _box_ref: i32) -> Result<i32> {
+    Ok(0) // todo: implement me
+}
+
+/// `java.lang.invoke.MethodHandleNatives.getMemberVMInfo(Ljava/lang/invoke/MemberName;)Ljava/lang/Object;`
+pub(crate) fn get_member_vm_info(member_name_ref: i32) -> Result<i32> {
+    // returns {vmindex,vmtarget}
+    let member_name = MemberName::new(member_name_ref)?;
+    member_name.get_member_vm_info()
+}
+
+/// `java.lang.invoke.MethodHandleNatives.setCallSiteTargetNormal(Ljava/lang/invoke/CallSite;Ljava/lang/invoke/MethodHandle;)V`
+pub(crate) fn set_call_site_target_normal(
+    call_site_ref: i32,
+    method_handle_ref: i32,
+) -> Result<()> {
+    let call_site_name = HEAP.get_instance_name(call_site_ref)?;
+    HEAP.set_object_field_value(
+        call_site_ref,
+        &call_site_name,
+        "target",
+        vec![method_handle_ref],
+    )
+}
--- src/vm/system_native/method_handle_natives/wrappers.rs
+++ src/vm/system_native/method_handle_natives/wrappers.rs
@@ -1,41 +1,12 @@
 use crate::vm::error::Result;
-use crate::vm::heap::heap::HEAP;
-use crate::vm::helper::{clazz_ref, i64_to_vec};
 use crate::vm::stack::stack_frame::StackFrames;
 use crate::vm::system_native::method_handle_natives::invocation::invoke_exact;
-use crate::vm::system_native::method_handle_natives::member_name::MemberName;
 use crate::vm::system_native::method_handle_natives::native_accessor::{
     native_accessor_invoke0, native_accessor_newinstance0,
 };
-use crate::vm::system_native::method_handle_natives::offsets::{
-    get_field_offset, get_static_field_offset,
-};
-use crate::vm::system_native::method_handle_natives::resolution::{member_name_init, resolve};
 use crate::vm::system_native::method_handle_natives::var_handle::{
     var_handle_compare_and_set, var_handle_get, var_handle_set,
 };
-
-pub(crate) fn method_handle_natives_init_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let member_name_ref = args[0];
-    let obj_ref = args[1];
-    member_name_init(member_name_ref, obj_ref)?;
-
-    Ok(vec![])
-}
-
-pub(crate) fn method_handle_natives_resolve_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let member_mame_ref = args[0];
-    let caller_class_ref = args[1];
-    let lookup_mode = args[2];
-    let speculative_resolve = args[3] != 0;
-    let member_mame_ref = resolve(
-        member_mame_ref,
-        caller_class_ref,
-        lookup_mode,
-        speculative_resolve,
-    )?;
-    Ok(vec![member_mame_ref])
-}
 
 pub(crate) fn method_handle_invoke_exact_wrp(
     args: &[i32],
@@ -54,12 +25,6 @@ pub(crate) fn method_handle_invoke_basic_wrp(
     method_handle_invoke_exact_wrp(args, stack_frames) // TODO: implement real invokeBasic
 }
 
-pub(crate) fn method_handle_natives_object_field_offset_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let member_name_ref = args[0];
-    let offset = get_field_offset(member_name_ref)?;
-    Ok(i64_to_vec(offset))
-}
-
 pub(crate) fn method_handle_invoke_wrp(
     args: &[i32],
     stack_frames: &mut StackFrames,
@@ -68,56 +33,6 @@ pub(crate) fn method_handle_invoke_wrp(
     // making this a temporary placeholder that could cause incorrect behavior.
     // TODO: implement real invoke
     method_handle_invoke_exact_wrp(args, stack_frames)
-}
-
-pub(crate) fn method_handle_natives_static_field_offset_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let member_name_ref = args[0];
-    let offset = get_static_field_offset(member_name_ref)?;
-    Ok(i64_to_vec(offset))
-}
-
-pub(crate) fn method_handle_natives_static_field_base_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let member_name_ref = args[0];
-    let base_ref = static_field_base(member_name_ref)?;
-    Ok(vec![base_ref])
-}
-fn static_field_base(member_name_ref: i32) -> Result<i32> {
-    let member_name = MemberName::new(member_name_ref)?;
-    let class_name = member_name.class_name();
-    clazz_ref(class_name)
-}
-
-// By not setting a value for `box` we break the check loop on the Java side so, just bypassing the check
-pub(crate) fn method_handle_natives_get_named_con_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let _what = args[0];
-    let _box_ref = args[1];
-    Ok(vec![0])
-}
-
-pub(crate) fn method_handle_natives_get_member_vm_info_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    // returns {vmindex,vmtarget}
-    let member_name_ref = args[0];
-    let member_name = MemberName::new(member_name_ref)?;
-    let array_ref = member_name.get_member_vm_info()?;
-    Ok(vec![array_ref])
-}
-
-pub(crate) fn set_call_site_target_normal_wrp(args: &[i32]) -> Result<Vec<i32>> {
-    let call_site_ref = args[0];
-    let method_handle_ref = args[1];
-
-    set_call_site_target_normal(call_site_ref, method_handle_ref)?;
-
-    Ok(vec![])
-}
-fn set_call_site_target_normal(call_site_ref: i32, method_handle_ref: i32) -> Result<()> {
-    let call_site_name = HEAP.get_instance_name(call_site_ref)?;
-    HEAP.set_object_field_value(
-        call_site_ref,
-        &call_site_name,
-        "target",
-        vec![method_handle_ref],
-    )
 }
 
 pub(crate) fn native_accessor_invoke0_wrp(args: &[i32]) -> Result<Vec<i32>> {
