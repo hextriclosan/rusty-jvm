6a4fc51cb4bc77ab36e7db2b2bb73b3883d09d59
invokestring <207432455+invokestring@users.noreply.github.com>
2026-09-17 23:36:42 +1000

    Implement JNI IsInstanceOf

--- src/vm/jni/jni_env.rs
+++ src/vm/jni/jni_env.rs
@@ -36,7 +36,7 @@ use crate::vm::jni::object_fields_impl::{
     set_boolean_field, set_byte_field, set_char_field, set_double_field, set_float_field,
     set_int_field, set_long_field, set_object_field, set_short_field,
 };
-use crate::vm::jni::object_operations_impl::{get_object_class, is_same_object};
+use crate::vm::jni::object_operations_impl::{get_object_class, is_instance_of, is_same_object};
 use crate::vm::jni::static_fields_impl::{
     get_static_boolean_field, get_static_byte_field, get_static_char_field,
     get_static_double_field, get_static_field_id, get_static_float_field, get_static_int_field,
@@ -164,7 +164,6 @@ jni_stub!(AllocObject(jclass) -> jobject);
 jni_variadic_stub!(NewObject, NewObject_ptr, (jclass, jmethodID) -> jobject);
 jni_stub!(NewObjectV(jclass, jmethodID, va_list) -> jobject);
 jni_stub!(NewObjectA(jclass, jmethodID, *const jvalue) -> jobject);
-jni_stub!(IsInstanceOf(jobject, jclass) -> jboolean);
 jni_variadic_stub!(CallObjectMethod, CallObjectMethod_ptr, (jobject, jmethodID) -> jobject);
 jni_stub!(CallObjectMethodV(jobject, jmethodID, va_list) -> jobject);
 jni_variadic_stub!(CallBooleanMethod, CallBooleanMethod_ptr, (jobject, jmethodID) -> jboolean);
@@ -278,7 +277,7 @@ static VTABLE: Wrapper = {
     ni.v24.NewObjectV = NewObjectV;
     ni.v24.NewObjectA = NewObjectA;
     ni.v24.GetObjectClass = get_object_class;
-    ni.v24.IsInstanceOf = IsInstanceOf;
+    ni.v24.IsInstanceOf = is_instance_of;
     ni.v24.GetMethodID = get_method_id;
     ni.v24.CallObjectMethod = CallObjectMethod_ptr;
     ni.v24.CallObjectMethodV = CallObjectMethodV;
