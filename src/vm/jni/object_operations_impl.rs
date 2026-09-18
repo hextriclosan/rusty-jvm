16ab3f651e49e173195bc110a9ccefc5a93c226e
invokestring <207432455+invokestring@users.noreply.github.com>
2026-09-18 12:12:23 +1000

    Implement JNI AllocObject

--- src/vm/jni/object_operations_impl.rs
+++ src/vm/jni/object_operations_impl.rs
@@ -1,6 +1,10 @@
 use crate::vm::heap::heap::HEAP;
-use crate::vm::helper::clazz_ref;
+use crate::vm::helper::{clazz_ref, klass};
+use crate::vm::jni::utils::set_pending_instantiation_exception;
+use crate::vm::method_area::method_area::with_method_area;
+use jclassmodel::modifiers::ClassModifier;
 use jni_sys::{jboolean, jclass, jobject, JNIEnv};
+use std::ptr::null_mut;
 
 pub(super) extern "system" fn get_object_class(_env: *mut JNIEnv, obj: jobject) -> jclass {
     let instance_name = HEAP
@@ -15,4 +19,19 @@ pub(super) extern "system" fn is_same_object(
     second: jobject,
 ) -> jboolean {
     (first == second) as jboolean
+}
+
+pub(super) extern "system" fn alloc_object(_env: *mut JNIEnv, clazz: jclass) -> jobject {
+    let target = klass(clazz as i32).expect("Failed to get class from reference");
+    let modifiers = target.class_modifiers();
+    if modifiers.intersects(ClassModifier::Interface | ClassModifier::Abstract) {
+        set_pending_instantiation_exception(target.this_class_name());
+        return null_mut();
+    }
+
+    let instance = with_method_area(|method_area| {
+        method_area.create_instance_with_default_fields(target.this_class_name())
+    })
+    .expect("Failed to allocate object");
+    HEAP.create_instance(instance) as jobject
 }
