cf177d0dab314103e2cfda6570d8e504ffbaaf9a
invokestring <207432455+invokestring@users.noreply.github.com>
2026-09-18 12:52:36 +1000

    Throw for null JNI string creation input

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4247,6 +4247,26 @@ WARNING: Restricted methods will be blocked in a future release unless native ac
 }
 
 #[test]
+fn should_throw_for_null_jni_string_creation_input() {
+    let lib_dir_path = format!("-Djava.library.path={}", env!("JNI_TEST_LIB_PATH"));
+    utils::assert_with_all_args(
+        &[&lib_dir_path],
+        "samples.javacore.loadlibrary.stringops.JniNewStringNullDemo",
+        &[],
+        "utf16 caught\nutf8 caught\n",
+        r#"WARNING: A restricted method in java.lang.System has been called
+WARNING: java.lang.System::loadLibrary has been called by samples.javacore.loadlibrary.stringops.JniNewStringNullDemo in an unnamed module
+WARNING: Use --enable-native-access=ALL-UNNAMED to avoid a warning for callers in this module
+WARNING: Restricted methods will be blocked in a future release unless native access is enabled
+
+"#,
+        Success,
+        0,
+        HashMap::default(),
+    );
+}
+
+#[test]
 fn should_support_thread_pool_executor() {
     assert_success(
         "samples.concurrency.advancedpooldemo.AdvancedPoolDemo",
